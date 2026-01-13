1840dcedf5add3841f74febbcaeb892fbd2c3d4f
camara9126 <acvision01@gmail.com>
2026-01-13 10:12:13 +0100

    mise a jour du 13/01/2026

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,15 @@ use App\Http\Controllers\PersonnelController;
 use App\Http\Controllers\UserController;
 use App\Models\Article;
 use App\Models\Menu;
+use App\Models\User;
+
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
 
 // page d'accueil
 Route::get('/', function () {
     $menus= Menu::all();
-    $plats= Menu::where('description','plats')->get();
+    $plats= Menu::where('description','plats')->latest()->get();
     $articles= Article::all();
     return view('home.index', compact('menus','articles','plats'));
 })->name('home');
@@ -23,12 +26,24 @@ Route::get('/doc/{id}', function ($id) {
     return view('home.test2', compact('articles'));
 });
 
+
+//apropos
+Route::get('/about', function () {
+   // $categorie= Categorie::all();
+    return view('home.about');
+})->name('about');
+
 //contact
 Route::get('/contact', function () {
    // $categorie= Categorie::all();
     return view('home.contact');
 })->name('contact');
 
+//service
+Route::get('/service', function () {
+   // $categorie= Categorie::all();
+    return view('home.service');
+})->name('service');
 
 //menu
 Route::get('/menu', function () {
@@ -36,6 +51,23 @@ Route::get('/menu', function () {
     return view('home.menu', compact('menus'));
 })->name('menu');
 
+//reservation
+Route::get('/reservation', function () {
+   // $categorie= Categorie::all();
+    return view('home.reservation');
+})->name('reservation');
+
+//equipe
+Route::get('/equipe', function () {
+   // $categorie= Categorie::all();
+    return view('home.equipe');
+})->name('equipe');
+
+//temoignage
+Route::get('/temoignage', function () {
+   // $categorie= Categorie::all();
+    return view('home.temoignage');
+})->name('temoignage');
 
 //Route connexion
 Route::resource('/login', UserController::class);
@@ -45,6 +77,8 @@ Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
 //Route panier
 Route::resource('/panier',CardController::class);
+Route::post('/ajout/panier', [CardController::class, 'store'])->name('panier.store');
+Route::delete('/ajout/{rowId}', [CardController::class, 'destroy'])->name('panier.destroy');
 Route::delete('/ajout/{rowId}', [CardController::class, 'delete'])->name('delete');
 
 // Route mise a jour commande panier
@@ -52,11 +86,13 @@ Route::get('/panier/plus/{rowId}', [CardController::class, 'plus'])->name('panie
 Route::get('/panier/moins/{rowId}', [CardController::class, 'moins'])->name('panier.moins');
 
 //Route personnel
-Route::resource('/personnel', PersonnelController::class)->middleware(['auth','verified']);
+Route::resource('/personnel', PersonnelController::class);
 
 //Article
 Route::resource('/darticle', ArticleController::class)->middleware(['auth','verified']);
-//Route::get('/article/{id}',[ArticleController::class, 'show'])->name('article.show');
+Route::get('/article/{id}',[ArticleController::class, 'show'])->name('article.show');
+Route::get('/darticle/{id}/edit',[ArticleController::class, 'edit'])->middleware(['auth','verified'])->name('darticle.edit');
+Route::patch('/darticle/{id}',[ArticleController::class, 'update'])->middleware(['auth','verified'])->name('darticle.update');
 
 
 //Dashboard
@@ -71,6 +107,8 @@ Route::get('/dhome', function () {
 //Menu
 Route::resource('/dmenu', MenuController::class)->middleware(['auth','verified']);
 Route::get('/menu/{id}',[MenuController::class, 'show'])->name('menu.show');
+Route::patch('/dmenu/{id}/edit',[MenuController::class, 'edit'])->middleware(['auth','verified'])->name('dmenu.edit');
+Route::patch('/dmenu/{id}',[MenuController::class, 'update'])->middleware(['auth','verified'])->name('dmenu.update');
 
 //Route whatsapp
 Route::get('/whatsapp', [CardController::class, 'whatsapp'])->name('cart.whatsapp');
